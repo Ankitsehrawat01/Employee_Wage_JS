4572cf72eae4d161b378fbb0b351d6e76daa36af
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-27 00:49:20 +0530

    Code Refactor

--- Employee.js
+++ Employee.js
@@ -1,5 +1,5 @@
 /*
-//UC1--Check Weather Employee is present or Absent
+UC1--Check Weather Employee is present or Absent
 
 const IS_ABSENT=0;
 
@@ -13,8 +13,8 @@ else
     console.log("Employee is Present");
 }
 */
-
-//UC2--Employee Daily Wage Calculation
+/*
+UC2--Employee Daily Wage Calculation
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -40,3 +40,30 @@ switch(empCheck) {
 
 let empWage = empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
+*/
+//UC3--Code Refactor
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+function getWorkingHours(empCheck){
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
+}
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage "+empWage);
